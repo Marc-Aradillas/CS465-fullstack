6f6ac9eeb9836a695852278c5bbefdae175c8369
Marc Aradillas <aradillasm@gmail.com>
2025-04-14 02:28:53 -0500

    Module 6 Completed Baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -52,7 +52,79 @@ const tripsFindByCode = async(req, res) => {
     
 };
 
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async (req, res) => {
+    try {
+        const q = await Model.create({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        });
+        return res.status(201).json(q);
+    } catch (err) {
+        console.log(err); // Log the error to understand the issue
+        return res.status(400).json({ message: "Error adding trip", error: err });
+    }
+};
+
+// PUT: /trips/:tripCode - Updates an existing trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async (req, res) => {
+
+    // Uncomment for debugging
+    console.log(req.params);
+    console.log(req.body);
+  
+    try {
+      const q = await Model
+        .findOneAndUpdate(
+          { 'code': req.params.tripCode },
+          {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+          },
+          { new: true } // returns the modified document rather than the original
+        )
+        .exec();
+  
+      if (!q) { // Database returned no data
+        return res
+          .status(400)
+          .json({ message: 'Trip not found or update failed' });
+      } else { // Return the resulting updated trip
+        return res
+          .status(200)
+          .json(q);
+      }
+    } catch (err) {
+      // Catch any errors and return a 500 internal server error
+      console.error(err);
+      return res
+        .status(500)
+        .json({ message: 'Error updating trip', error: err });
+    }
+  
+    // Uncomment the following line to show results of operation
+    // on the console
+    // console.log(q);
+  };
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
